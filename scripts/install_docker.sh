51db77564bd93b548b31958284dac34893e00926
DINESH S <dineshpy07@gmail.com>
2018-11-20 16:17:10 +0530

    Add install_docker.sh script

---
+++ scripts/install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo docker run hello-world
+
+# Post installation steps
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install python-pip
+sudo apt-get install python-pip
+
+# Install docker-compose
+pip install docker-compose
